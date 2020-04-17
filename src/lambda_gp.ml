14fcbc1b7c0e2bcb5ee2d02bc4c398d15934ce20
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-04-17 17:37:48 +0200

    or example

--- src/lambda_gp.ml
+++ src/lambda_gp.ml
@@ -2,8 +2,6 @@ open Lambda;;
 open Genetic;;
 open Bool;;
 
-
-
 (** Example 1: not function *)
 (* let not_fitness t b =
   let res = reduce 4 @@ App(t, if b then ltrue else lfalse) in
@@ -50,6 +48,34 @@ let s = ga_init {
     (and_fitness t true false) *.
     (and_fitness t false false) *.
     (and_fitness t false true)
+  );
+  valid_f= (fun t -> Bool.is_bool @@ reduce 4 (App(App(t, lfalse), lfalse)));
+} in ga_print s; ga_steps s; *)
+
+
+(** Example 3: or function *)
+(* let or_fitness t b b' =
+  let res = reduce 4 @@ App(App(t, if b then ltrue else lfalse), if b' then ltrue else lfalse) in
+  match b, b', res with
+      false, true, ltrue -> 1.0
+    | true, false, ltrue -> 1.0
+    | false, false, lfalse -> 1.0
+    | true, true, lfalse -> 1.0
+    | _, _, _ -> 0.0
+;;
+
+let s = ga_init {
+  pop_size= 32;
+  term_len= 6;
+  var_n= 2;
+  gen_n=1000;
+  fitness_target= 1.0;
+  test_best_f= (fun t -> (or_fitness t (Random.bool ()) (Random.bool ())) = 1.0);
+  fitness_f= (fun t -> 
+    (or_fitness t true true) *. 
+    (or_fitness t true false) *.
+    (or_fitness t false false) *.
+    (or_fitness t false true)
   );
   valid_f= (fun t -> Bool.is_bool @@ reduce 4 (App(App(t, lfalse), lfalse)));
 } in ga_print s; ga_steps s; *)
